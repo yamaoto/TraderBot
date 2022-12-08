de2b41b8ff389a791d359c18a70c1a750f26dc16
Anatoliy Mikhailov <yamaoto@mail.ru>
2022-12-08 15:48:09 +0700

    Enable gRPC-Web at Admin

--- TraderBot.Admin/Program.cs
+++ TraderBot.Admin/Program.cs
@@ -9,14 +9,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
+builder.Services.AddGrpc();
+
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+    builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+}));
+
 await builder.Services.AddAndConfigureRavenDbAsync(builder.Configuration);
 
 var app = builder.Build();
 
-// app.UseGrpcWeb();
+app.UseGrpcWeb();
 
-app.MapGrpcService<AdminService>();
-    // .EnableGrpcWeb();
+app.UseCors("AllowAll");
 
+app.MapGrpcService<AdminService>()
+    .EnableGrpcWeb()
+    .RequireCors("AllowAll");
 
 app.Run();
